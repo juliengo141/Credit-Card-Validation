496f9f7bd650785ebe57e6c293f532d3e873d965
Julie <46454827+juliengo141@users.noreply.github.com>
2019-07-27 14:40:01 -0400

    Add files via upload

---
+++ Credit Card Validation/Credit Card Validation/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Credit_Card_Validation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.verifyButton = new System.Windows.Forms.Button();
+            this.IsValidLabel = new System.Windows.Forms.Label();
+            this.cardTypeLabel = new System.Windows.Forms.Label();
+            this.VisaButton = new System.Windows.Forms.Button();
+            this.MasterCardButton = new System.Windows.Forms.Button();
+            this.AmericanExpressButton = new System.Windows.Forms.Button();
+            this.DiscoverButton = new System.Windows.Forms.Button();
+            this.DinersClubButton = new System.Windows.Forms.Button();
+            this.creditCardGeneratorLabel = new System.Windows.Forms.Label();
+            this.creditCardNumberIsLabel = new System.Windows.Forms.Label();
+            this.generatedCreditCardNumberLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(28, 28);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.inputTextBox.MaxLength = 16;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(175, 22);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AcceptsOnlyNumbers_KeyPress);
+            // 
+            // verifyButton
+            // 
+            this.verifyButton.Location = new System.Drawing.Point(231, 28);
+            this.verifyButton.Margin = new System.Windows.Forms.Padding(4);
+            this.verifyButton.Name = "verifyButton";
+            this.verifyButton.Size = new System.Drawing.Size(100, 28);
+            this.verifyButton.TabIndex = 1;
+            this.verifyButton.Text = "Verify";
+            this.verifyButton.UseVisualStyleBackColor = true;
+            this.verifyButton.Click += new System.EventHandler(this.VerifyButton_Click);
+            // 
+            // IsValidLabel
+            // 
+            this.IsValidLabel.AutoSize = true;
+            this.IsValidLabel.Location = new System.Drawing.Point(24, 73);
+            this.IsValidLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.IsValidLabel.Name = "IsValidLabel";
+            this.IsValidLabel.Size = new System.Drawing.Size(178, 17);
+            this.IsValidLabel.TabIndex = 2;
+            this.IsValidLabel.Text = "The credit card number is :";
+            // 
+            // cardTypeLabel
+            // 
+            this.cardTypeLabel.AutoSize = true;
+            this.cardTypeLabel.Location = new System.Drawing.Point(24, 95);
+            this.cardTypeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.cardTypeLabel.Name = "cardTypeLabel";
+            this.cardTypeLabel.Size = new System.Drawing.Size(98, 17);
+            this.cardTypeLabel.TabIndex = 3;
+            this.cardTypeLabel.Text = "Type of Card :";
+            // 
+            // VisaButton
+            // 
+            this.VisaButton.Location = new System.Drawing.Point(29, 161);
+            this.VisaButton.Name = "VisaButton";
+            this.VisaButton.Size = new System.Drawing.Size(88, 23);
+            this.VisaButton.TabIndex = 4;
+            this.VisaButton.Text = "Visa";
+            this.VisaButton.UseVisualStyleBackColor = true;
+            this.VisaButton.Click += new System.EventHandler(this.VisaButton_Click);
+            // 
+            // MasterCardButton
+            // 
+            this.MasterCardButton.Location = new System.Drawing.Point(132, 161);
+            this.MasterCardButton.Name = "MasterCardButton";
+            this.MasterCardButton.Size = new System.Drawing.Size(93, 23);
+            this.MasterCardButton.TabIndex = 5;
+            this.MasterCardButton.Text = "Master Card";
+            this.MasterCardButton.UseVisualStyleBackColor = true;
+            this.MasterCardButton.Click += new System.EventHandler(this.MasterCardButton_Click);
+            // 
+            // AmericanExpressButton
+            // 
+            this.AmericanExpressButton.Location = new System.Drawing.Point(189, 195);
+            this.AmericanExpressButton.Name = "AmericanExpressButton";
+            this.AmericanExpressButton.Size = new System.Drawing.Size(142, 23);
+            this.AmericanExpressButton.TabIndex = 6;
+            this.AmericanExpressButton.Text = "American Express";
+            this.AmericanExpressButton.UseVisualStyleBackColor = true;
+            this.AmericanExpressButton.Click += new System.EventHandler(this.AmericanExpressButton_Click);
+            // 
+            // DiscoverButton
+            // 
+            this.DiscoverButton.Location = new System.Drawing.Point(239, 161);
+            this.DiscoverButton.Name = "DiscoverButton";
+            this.DiscoverButton.Size = new System.Drawing.Size(92, 23);
+            this.DiscoverButton.TabIndex = 7;
+            this.DiscoverButton.Text = "Discover";
+            this.DiscoverButton.UseVisualStyleBackColor = true;
+            this.DiscoverButton.Click += new System.EventHandler(this.DiscoverButton_Click);
+            // 
+            // DinersClubButton
+            // 
+            this.DinersClubButton.Location = new System.Drawing.Point(27, 195);
+            this.DinersClubButton.Name = "DinersClubButton";
+            this.DinersClubButton.Size = new System.Drawing.Size(144, 23);
+            this.DinersClubButton.TabIndex = 8;
+            this.DinersClubButton.Text = "Diners Club";
+            this.DinersClubButton.UseVisualStyleBackColor = true;
+            this.DinersClubButton.Click += new System.EventHandler(this.DinersClubButton_Click);
+            // 
+            // creditCardGeneratorLabel
+            // 
+            this.creditCardGeneratorLabel.AutoSize = true;
+            this.creditCardGeneratorLabel.Location = new System.Drawing.Point(26, 133);
+            this.creditCardGeneratorLabel.Name = "creditCardGeneratorLabel";
+            this.creditCardGeneratorLabel.Size = new System.Drawing.Size(234, 17);
+            this.creditCardGeneratorLabel.TabIndex = 10;
+            this.creditCardGeneratorLabel.Text = "Generate a Credit Card Number for:";
+            // 
+            // creditCardNumberIsLabel
+            // 
+            this.creditCardNumberIsLabel.AutoSize = true;
+            this.creditCardNumberIsLabel.Location = new System.Drawing.Point(29, 232);
+            this.creditCardNumberIsLabel.Name = "creditCardNumberIsLabel";
+            this.creditCardNumberIsLabel.Size = new System.Drawing.Size(174, 17);
+            this.creditCardNumberIsLabel.TabIndex = 11;
+            this.creditCardNumberIsLabel.Text = "The credit card number is:";
+            // 
+            // generatedCreditCardNumberLabel
+            // 
+            this.generatedCreditCardNumberLabel.AutoSize = true;
+            this.generatedCreditCardNumberLabel.Location = new System.Drawing.Point(32, 253);
+            this.generatedCreditCardNumberLabel.Name = "generatedCreditCardNumberLabel";
+            this.generatedCreditCardNumberLabel.Size = new System.Drawing.Size(0, 17);
+            this.generatedCreditCardNumberLabel.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 292);
+            this.Controls.Add(this.generatedCreditCardNumberLabel);
+            this.Controls.Add(this.creditCardNumberIsLabel);
+            this.Controls.Add(this.creditCardGeneratorLabel);
+            this.Controls.Add(this.DinersClubButton);
+            this.Controls.Add(this.DiscoverButton);
+            this.Controls.Add(this.AmericanExpressButton);
+            this.Controls.Add(this.MasterCardButton);
+            this.Controls.Add(this.VisaButton);
+            this.Controls.Add(this.cardTypeLabel);
+            this.Controls.Add(this.IsValidLabel);
+            this.Controls.Add(this.verifyButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Credit Card Validation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button verifyButton;
+        private System.Windows.Forms.Label IsValidLabel;
+        private System.Windows.Forms.Label cardTypeLabel;
+        private System.Windows.Forms.Button VisaButton;
+        private System.Windows.Forms.Button MasterCardButton;
+        private System.Windows.Forms.Button AmericanExpressButton;
+        private System.Windows.Forms.Button DiscoverButton;
+        private System.Windows.Forms.Button DinersClubButton;
+        private System.Windows.Forms.Label creditCardGeneratorLabel;
+        private System.Windows.Forms.Label creditCardNumberIsLabel;
+        private System.Windows.Forms.Label generatedCreditCardNumberLabel;
+    }
+}
+
---
+++ Credit Card Validation/Credit Card Validation/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Credit_Card_Validation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Global Variables
+
+        string[] cardTypes = new string[] { "Visa", "MasterCard", "AmericanExpress", "Discover", "DinersClub" };
+
+        #endregion
+
+        #region Event Handler
+        private void AcceptsOnlyNumbers_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void VerifyButton_Click(object sender, EventArgs e)
+        {
+            int typeOfCard = CardType(inputTextBox.Text);
+            bool checkSumDivisibleBy10 = CheckSum(inputTextBox.Text);
+
+            if (typeOfCard >=0 && checkSumDivisibleBy10==true)
+            {
+                IsValidLabel.Text = "The credit card number is valid";
+                cardTypeLabel.Text = "The type of card is: " + cardTypes[typeOfCard];
+
+            }
+            else
+            {
+                IsValidLabel.Text = "The credit card number is not valid";
+            }
+                
+        }
+
+        private void VisaButton_Click(object sender, EventArgs e)
+        {
+            Random randomGenerator = new Random();
+            string[] visaNumber = new string[16];
+            visaNumber[0] = "4";
+
+            for (int i=1; i<15; i++)
+            {
+                visaNumber[i] = randomGenerator.Next(1, 10).ToString();
+            }
+            visaNumber[15] = "0";
+
+            string cardNumber = "";
+
+            for (int i=0; i<16; i++)
+            {
+                cardNumber += visaNumber[i];
+            }
+
+            visaNumber[15] = LastDigit(cardNumber);
+
+            cardNumber = "";
+
+            for (int i = 0; i < 16; i++)
+            {
+                cardNumber += visaNumber[i];
+            }
+
+            generatedCreditCardNumberLabel.Text = cardNumber;            
+        }
+
+        private void MasterCardButton_Click(object sender, EventArgs e)
+        {
+            Random randomGenerator = new Random();
+            string[] masterCardNumber = new string[16];
+            masterCardNumber[0] = "5";
+            string[] cardDigit = new string[2] { "1", "5" };
+
+            masterCardNumber[1] = cardDigit[randomGenerator.Next(0, 2)];
+
+            for (int i = 2; i < 15; i++)
+            {
+                masterCardNumber[i] = randomGenerator.Next(1, 10).ToString();
+            }
+            masterCardNumber[15] = "0";
+
+            string cardNumber = "";
+
+            for (int i = 0; i < 16; i++)
+            {
+                cardNumber += masterCardNumber[i];
+            }
+
+            masterCardNumber[15] = LastDigit(cardNumber);
+
+            cardNumber = "";
+
+            for (int i = 0; i < 16; i++)
+            {
+                cardNumber += masterCardNumber[i];
+            }
+
+            generatedCreditCardNumberLabel.Text = cardNumber;
+        }
+
+        private void DiscoverButton_Click(object sender, EventArgs e)
+        {
+            Random randomGenerator = new Random();
+            string[] discoverNumber = new string[16];
+            discoverNumber[0] = "6";
+            discoverNumber[1] = "0";
+            discoverNumber[2] = "1";
+            discoverNumber[3] = "1";
+ 
+            for (int i = 4; i < 15; i++)
+            {
+                discoverNumber[i] = randomGenerator.Next(1, 10).ToString();
+            }
+            discoverNumber[15] = "0";
+
+            string cardNumber = "";
+
+            for (int i = 0; i < 16; i++)
+            {
+                cardNumber += discoverNumber[i];
+            }
+
+            discoverNumber[15] = LastDigit(cardNumber);
+
+            cardNumber = "";
+
+            for (int i = 0; i < 16; i++)
+            {
+                cardNumber += discoverNumber[i];
+            }
+
+            generatedCreditCardNumberLabel.Text = cardNumber;
+        }
+
+        private void DinersClubButton_Click(object sender, EventArgs e)
+        {
+            Random randomGenerator = new Random();
+            string[] dinersClubNumber = new string[14];
+            dinersClubNumber[0] = "3";
+            dinersClubNumber[1] = "6";
+
+            for (int i = 2; i < 13; i++)
+            {
+                dinersClubNumber[i] = randomGenerator.Next(1, 10).ToString();
+            }
+            dinersClubNumber[13] = "0";
+
+            string cardNumber = "";
+
+            for (int i = 0; i < 14; i++)
+            {
+                cardNumber += dinersClubNumber[i];
+            }
+
+            dinersClubNumber[13] = LastDigit(cardNumber);
+
+            cardNumber = "";
+
+            for (int i = 0; i < 14; i++)
+            {
+                cardNumber += dinersClubNumber[i];
+            }
+
+            generatedCreditCardNumberLabel.Text = cardNumber;
+        }
+
+        private void AmericanExpressButton_Click(object sender, EventArgs e)
+        {
+            Random randomGenerator = new Random();
+            string[] americanExpress = new string[15];
+            americanExpress[0] = "3";
+            string[] cardDigit = new string[2] { "4", "7" };
+
+            americanExpress[1] = cardDigit[randomGenerator.Next(0, 2)];
+
+            for (int i = 2; i < 14; i++)
+            {
+                americanExpress[i] = randomGenerator.Next(1, 10).ToString();
+            }
+            americanExpress[14] = "0";
+
+            string cardNumber = "";
+
+            for (int i = 0; i < 15; i++)
+            {
+                cardNumber += americanExpress[i];
+            }
+
+            americanExpress[14] = LastDigit(cardNumber);
+
+            cardNumber = "";
+
+            for (int i = 0; i < 15; i++)
+            {
+                cardNumber += americanExpress[i];
+            }
+
+            generatedCreditCardNumberLabel.Text = cardNumber;
+        }
+    
+        #endregion
+
+        #region Methods
+
+        private static int CardType (string creditCardDigits)
+        {
+            
+            int numberOfDigits= creditCardDigits.Length;
+            int card = -1;
+            string firstTwoDigits = creditCardDigits.Substring(0, 2);
+
+            if (numberOfDigits == 16)
+            {                
+
+                if (creditCardDigits.Substring(0, 1) == "4")
+                {
+                    card = 0;                    
+                }
+                
+                else if (firstTwoDigits == "51" || firstTwoDigits == "55")
+                {
+                    card = 1;
+                }
+
+                else if (creditCardDigits.Substring(0,4)=="6011")
+                {
+                    card = 3;
+                }
+                else
+                {
+                    card = -1;
+                }
+
+            }
+            else if (numberOfDigits==15)
+            {
+                if (firstTwoDigits == "34" || firstTwoDigits == "37")
+                {
+                    card = 2;
+                }
+                else card = -1;
+            }     
+       
+            else if (numberOfDigits==14)
+            {
+                if (firstTwoDigits == "36")
+                {
+                    card = 4;
+                }
+                else
+                {
+                    card = -1;
+                }
+            }
+            else
+            {
+                card = -1;
+            }
+
+            return card;
+
+        }
+
+        private static bool CheckSum (string creditCardDigits)
+        {
+            int sum = 0;
+
+            for (int i = creditCardDigits.Length - 1; i > 0; i-=2 )
+            {
+                int oddDigit = Convert.ToInt32(creditCardDigits.Substring(i, 1));
+                int evenDigit = Convert.ToInt32(creditCardDigits.Substring(i-1, 1));
+
+                sum += oddDigit;
+             
+                int product = evenDigit * 2;
+                if (product <= 9)
+                {
+                    sum += product;
+                }
+                else
+                {
+                    sum += (product / 10 + product % 10);
+                }                           
+                
+            }
+            if (sum%10==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static string LastDigit(string creditCardDigits)
+        {
+            int sum = 0;
+
+            for (int i = creditCardDigits.Length - 1; i > 0; i -= 2)
+            {
+                int oddDigit = Convert.ToInt32(creditCardDigits.Substring(i, 1));
+                int evenDigit = Convert.ToInt32(creditCardDigits.Substring(i - 1, 1));
+
+                sum += oddDigit;
+
+                int product = evenDigit * 2;
+                if (product <= 9)
+                {
+                    sum += product;
+                }
+                else
+                {
+                    sum += (product / 10 + product % 10);
+                }
+
+            }
+            return (10-(sum % 10)).ToString();
+        }
+
+
+
+
+
+
+        #endregion
+
+        
+    }
+}
